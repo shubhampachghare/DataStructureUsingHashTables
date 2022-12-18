4b970b30f4910c3c521354f9b267369545cb6288
shubhampachghare <shubhampachghare@gmail.com>
2022-12-19 00:54:50 +0530

    [Shubham]-Permutation of string function

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AlgorithmPrograms
+{
+    public class UC1_Permutation
+    {
+
+        public static void Permute(string S, int l, int r)
+        {
+            if (l == r)
+                Console.WriteLine(S);
+            else
+            {
+                for (int i = l; i <= r; i++)
+                {
+                    S = swap(S, l, r);
+                    Permute(S, l + 1, r);
+                    S = swap(S, l, i);
+                }
+            }
+        }
+        public static string swap(string a, int i, int j)
+        {
+            char T;
+            char[] charArray = a.ToCharArray();
+            T = charArray[i];
+            charArray[i] = charArray[j];
+            charArray[j] = T;
+            string s = new string(charArray);
+            return s;
+        }
+    }
+}
